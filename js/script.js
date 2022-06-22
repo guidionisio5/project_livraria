d6df9974b5151d6ce518db4c5eb46966fa28f9f1
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-06-21 22:29:40 -0300

    iniciando javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Criar uma Funçãp em JavaScript
+// Formato ES6 baseado em Arrow Function
+
+// Função que realiza o cadastro de livros
+const cadastrar = () => {
+    
+    // Captura o valor digitado no campo titulo
+    let titulo = document.getElementById('titulo').value
+
+    alert(titulo)
+}
